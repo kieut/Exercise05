6d573cc3fb97260f43546c378a744574e2458a34
Kieu Dung Tran <kieudung.tran@gmail.com>
2014-06-07 17:03:52 -0700

    Update lettercount.py

--- lettercount.py
+++ lettercount.py
@@ -1,20 +1,4 @@
-'''
 
-should I convert all characters to lower case, then use ord() to convert to ascii numbers.
-
-create list of alphabet outside of function
-
-def count_letters():
-	open file
-	read file 1 character at a time
-	
-	count = 0
-	for each character in filetext:
-		convert to lower case, then use ascii numbers??
-		if char == letter
-s'''
-		
-		
 from sys import argv
 
 script, filename = argv
